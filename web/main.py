0e865ac60f0a68cd07714350e48def2f4ca25416
Yisaaaa <ryansanisit19@gmail.com>
2025-05-13 11:05:52 +0800

    Add dummy servers

---
+++ web/main.py
@@ -0,0 +1,24 @@
+from ..common.db import Database
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+import logging
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+  # Server startup 
+
+
+  db = Database()
+  logger = logging.getLogger("uvicorn")
+  await db.initialize_connection()
+  
+  yield
+  # Server shutdown
+
+  await db.close_all()
+
+app = FastAPI(lifespan=lifespan) 
+
+@app.get("/api/web/dummy")
+async def dummy_server():
+  return "Hello, Web World!"
